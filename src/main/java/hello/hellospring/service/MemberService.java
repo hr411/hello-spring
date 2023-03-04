4aa14a71023b21f0669020ba2c9020639478ec7d
Hyorin Song <hrsong411@gmail.com>
2023-03-04 16:25:22 +0900

    AOP

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -25,10 +25,9 @@ public class MemberService {
      * 회원가입
      */
     public Long join(Member member){
-        validateDuplicateMember(member);
-
-        memberRepository.save(member);
-        return member.getId();
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
